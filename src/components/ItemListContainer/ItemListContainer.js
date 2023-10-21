b4867b91f68cfe0079a6933d0860dc62ddf79f00
Your Name <you@example.com>
2023-10-21 20:27:18 -0300

    Se conecta firebase-ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,31 +1,59 @@
 import { useState, useEffect } from "react"
-import { getProducts, getProductsByCategory} from '../../asyncMock'
+// import { getProducts, getProductsByCategory} from '../../asyncMock'
 import ItemList from "../ItemList/ItemList"
-
 import { useParams } from "react-router-dom"
+
+import { collection, getDocs, query, where } from "firebase/firestore"
+import { db } from "../../services/firebase"
+
+
 
 const ItemListContainer = ({ greeting }) => {
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const { categoryId } = useParams()
 
-    const { categoryId}= useParams()
+
+    // useEffect(() => {
+    //     const asyncFunc = categoryId ? getProductsByCategory : getProducts
+
+    //     asyncFunc(categoryId)
+    //         .then(response => {
+    //             setProducts(response)
+    //         })
+    //         .catch(error => {
+    //             console.error(error)
+    //         })
+    // }, [categoryId])
 
     useEffect(() => {
-        const asyncFunc = categoryId ? getProductsByCategory : getProducts
+        setLoading(true)
+        const coleccionProductos = categoryId ? query(collection (db, "items"), where("category", "==", categoryId)):collection (db, "items")
+        getDocs(coleccionProductos)
+            .then((res) => {
+                const list = res.docs.map((products)=>{
+                    return{
+                        id:products.id,
+                        ...products.data()
+                    }
+                })
+                setProducts(list)
+            })
 
-        asyncFunc(categoryId)
-            .then(response => {
-                setProducts(response)
-            })
-            .catch(error => {
-                console.error(error)
-            })
+            .catch((error) => console.log(error))
+            .finally(() => setLoading(false))
     }, [categoryId])
 
 
-    return(
+    return (
         <div>
-            <h1>{greeting}</h1>
-            <ItemList products={products}/>
+            {
+                loading ? <p>Cargando...</p>
+                    : <div>
+                        <h1>{greeting} <span> {categoryId && categoryId} </span> </h1>
+                        <ItemList products={products} />
+                    </div>
+            }
         </div>
     )
 }
