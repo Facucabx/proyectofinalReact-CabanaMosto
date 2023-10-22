28ab1f963e611b936ea8156a766c2827ba5ed85b
Your Name <you@example.com>
2023-10-21 21:32:03 -0300

    Se conecta firebase a ItemDetailContainer.js

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,27 +1,38 @@
 import './ItemDetailContainer.css'
-import { useState, useEffect } from 'react'
-import { getProductById } from '../../asyncMock'
+import React, { useState, useEffect } from 'react'
 import ItemDetail from '../ItemDetail/ItemDetail'
 import { useParams } from 'react-router-dom'
+import { collection, doc, getDoc } from 'firebase/firestore'
+import { db } from '../../services/firebase'
 
 const ItemDetailContainer = () => {
-    const [product, setProduct] = useState(null)
-
+    const [product, setProduct] = useState({})
+    const [loader, setLoader] = useState(false)
     const { itemId } = useParams()
 
+    // useEffect(() => {
+    //     getProductById(itemId)
+    //         .then(response => {
+    //             setProduct(response)
+    //         })
+    //         .catch(error => {
+    //             console.error(error)
+    //         })
+    // }, [itemId])
+
     useEffect(() => {
-        getProductById(itemId)
-            .then(response => {
-                setProduct(response)
-            })
-            .catch(error => {
-                console.error(error)
-            })
+        setLoader(true);
+        const collectionProd = collection(db, 'items');
+        const referenciaAlDoc = doc(collectionProd, itemId);
+        getDoc(referenciaAlDoc)
+            .then((res) => setProduct({ id: res.id, ...res.data() }))
+            .catch((error) => console.log(error))
+            .finally(() => setLoader(false))
     }, [itemId])
 
     return (
         <div className='ItemDetailContainer'>
-            <ItemDetail  {...product} />
+            {loader ? <p>Cargando...</p> : <ItemDetail  producto={product} />}            
         </div>
     )
 }
